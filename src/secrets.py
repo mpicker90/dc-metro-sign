4cdf019f4c0cdac6111f7ea2bf1af6e3eebe669c
mpicker90 <mpicker90@gmail.com>
2022-03-03 08:58:12 -0500

    fixing secrets and config

--- src/secrets.py
+++ src/secrets.py
@@ -2,9 +2,7 @@ from config import config
 
 # This file is required by the Matrix Portal library to properly
 # initialize the WiFi chip on the board.
-# 
-# Why isn't aren't the SSID and password just parameters for the
-# connect function? The world may never know :(
+
 secrets = {
 	'ssid': '2Gh wifi name here',
 	'password': 'wifi password here',
