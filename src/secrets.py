3d3a443bc06c2fba0fb6ba32d5660b045e3a9f3d
mpicker90 <mpicker90@gmail.com>
2022-03-04 13:03:34 -0500

    display full names when changing stations

--- src/secrets.py
+++ src/secrets.py
@@ -7,5 +7,7 @@ secrets = {
 	'ssid': '2Gh wifi name here',
 	'password': 'wifi password here',
 	'metro_api_key': 'api key here',
-	'openweather_token': 'open weather key here'
+	'openweather_token': 'open weather key here',
+	'aio_username': '_your_aio_username_',
+	'aio_key': '_your_big_huge_super_long_aio_key_'
 }
